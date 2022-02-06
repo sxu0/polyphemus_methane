fbae2ce353ae182540c10b2bbc56e9dd2d2d3c5e
sxu0 <sxu0726@gmail.com>
2022-02-06 03:28:13 -0500

    set up main file for running sims for petrolia

---
+++ src/petrolia_predictions.py
@@ -0,0 +1,34 @@
+"""
+petrolia_predictions.py
+
+Models predicted plumes for Petrolia landfill based on a range of inputs.
+
+Author: Shiqi Xu
+"""
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+import os
+
+import matplotlib
+matplotlib.use("Agg")
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from pathlib2 import Path
+
+from libs import coord_convert
+
+
+if __name__ == "__main__":
+
+    ## timestamp for distinguishing outputs
+    time_run = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+    ## source info
+    src_info = pd.read_csv(
+        Path.cwd() / "data" / "petrolia_landfill_source_coords.csv",
+        index_col="source location"
+    )
